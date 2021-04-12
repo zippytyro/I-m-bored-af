954df88c560faf9ce4fa90f3e9fecb255fc452f5
Shashwat <shashwatverma888@gmail.com>
2021-04-13 01:05:11 +0530

    API base done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const os = require("os");
 const path = require("path");
+const data = require("./data");
 const app = express();
 
 
@@ -19,6 +20,12 @@ app.get("/", (req, res)=>{
     res.sendFile("index.html");
 });
 
+app.get("/api", (req, res)=>{
+    let length = data.length;
+    let random = Math.floor(Math.random()*length)+1;
+    let activity = data[random];
+    res.send(JSON.stringify(activity));
+});
 
 app.listen(PORT || 3000, ()=>{
     console.log("Server running at 3000");
